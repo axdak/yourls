d014b797a7889384e00026200f2b7ab2a2551011
feiniaoaxi <535198526@qq.com>
2017-11-05 17:50:57 +0800

    Add files via upload

---
+++ Install/config.ini.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined('THINK_PATH')) exit();
+return array(
+	'SOFT_NAME' =>'短网址',
+	'SOFT_HOMEPAGE' =>'www.pengyong.info',
+	'SOFT_AUTHOR' =>'将军',
+	'SOFT_CONTRACT' =>'634150845',
+	'SOFT_VERSION'=>'3.0 运营版',
+	'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'~dbhost~',
+	'DB_NAME'=>'~dbname~',
+	'DB_USER'=>'~dbuser~',
+	'DB_PWD'=>'~dbpwd~',
+	'DB_PORT'=>'3306',
+	'DB_PREFIX'=>'~dbprefix~',
+	'DB_CHARSET'=>'~dblang~',
+	'DEFAULT_CHARSET'=>'~dblang~',
+	'COOKIE_PREFIX'=>'~dbcookie~'.'_', 
+	);
+?>
